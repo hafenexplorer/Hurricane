07090f2e5572fdab79769aa290789f932d0ec0f8
hafenexplorer <ignitedinnovations767@gmail.com>
2025-12-04 08:12:57 -0500

    Alchemy update test

--- src/haven/Sprite.java
+++ src/haven/Sprite.java
@@ -40,6 +40,9 @@ public abstract class Sprite implements RenderTree.Node, PView.Render2D {
     public static List<Factory> factories = new LinkedList<Factory>();
     static {
 	factories.add(SpriteLink.sfact);
+    factories.add(SkelSprite.fact);
+    factories.add(AnimSprite.fact);
+    factories.add(StaticSprite.fact);
 	factories.add(ModSprite.fact);
 	factories.add(AudioSprite.fact);
     }
